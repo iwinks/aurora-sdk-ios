a5b5d21360ec7b595918171c8ac6ba9f7742f0f5
Rafael Nobre <nobre84@gmail.com>
2016-11-17 23:56:56 -0200

    Basic BLE lib infrastructure.

---
+++ Example/Pods/Target Support Files/iwinks-ble-core-ios/iwinks-ble-core-ios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_iwinks_ble_core_ios : NSObject
+@end
+@implementation PodsDummy_iwinks_ble_core_ios
+@end
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,50 +1,26 @@
 // https://github.com/Quick/Quick
 
-import Quick
+import XCTest
 import Nimble
-import iwinks-ble-core-ios
+@testable import iwinks_ble_core_ios
 
-class TableOfContentsSpec: QuickSpec {
-    override func spec() {
-        describe("these will fail") {
-
-            it("can do maths") {
-                expect(1) == 2
-            }
-
-            it("can read") {
-                expect("number") == "string"
-            }
-
-            it("will eventually fail") {
-                expect("time").toEventually( equal("done") )
-            }
-            
-            context("these will pass") {
-
-                it("can do maths") {
-                    expect(23) == 23
-                }
-
-                it("can read") {
-                    expect("🐮") == "🐮"
-                }
-
-                it("will eventually pass") {
-                    var time = "passing"
-
-                    dispatch_async(dispatch_get_main_queue()) {
-                        time = "done"
-                    }
-
-                    waitUntil { done in
-                        NSThread.sleepForTimeInterval(0.5)
-                        expect(time) == "done"
-
-                        done()
-                    }
-                }
-            }
-        }
+class BLETests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
     }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testBasicMatcher() {
+        expect(1) == 1
+    }
+
+    func testBasicFailure() {
+        expect(2) != 1
+    }
+
 }
+
